5e51b60ebc7109a4ae30b716f1bb028fa9b2b900
Jari Kuikanmaki <jarikuikanmaki@jaris-imac.home>
2013-07-29 14:35:31 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuikanmaki::Application.config.secret_token = '7fe62dfa0eaf2d6006a1f13a0f40d8acd3da14b1429baa252bd8eb92ec3cb9aab021f1e7b510e8dd8c559cca3430bd84f05d4c7237af76cfe48ffd107ac3d5d5'
